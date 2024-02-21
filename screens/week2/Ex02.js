6632dee1f51cf7dce22241f69229d76b70852784
sscpla <sasicha.phu@dome.tu.ac.th>
2024-02-21 08:54:59 +0700

    ex2

--- screens/week2/Ex02.js
+++ screens/week2/Ex02.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function Ex01() {
+export default function Ex02() {
   return (
     <View style={{flex:1,flexDirection:'row'}}>
       <View style={{backgroundColor:'#50E3C2',width:100}}></View>
